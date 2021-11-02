6a32047b82ec267da9ce93f827c634b14c3d31a9
livk1375632510 <chenyu1999>
2021-11-02 16:34:14 +0800

    feat: 开始重写

--- kris-admin-route/src/main/java/com/kris/admin/route/mapper/DynamicRouteMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kris.admin.route.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kris.admin.route.model.DynamicRoute;
-import org.apache.ibatis.annotations.Mapper;
-
-/** @Author: kris @Date: 2021/7/6 @Description: ${Description} @Since: JDK11 */
-@Mapper
-public interface DynamicRouteMapper extends BaseMapper<DynamicRoute> {}
