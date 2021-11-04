719aa23a24f4bc111bdc632fa73816606546d1c8
livk1375632510 <chenyu1999>
2021-11-04 11:44:30 +0800

    feat: SpringBoot Admin整合、使用nacos注册中心

---
+++ livk-api-monitor/src/main/java/com/livk/cloud/api/LivkMonitor.java
@@ -0,0 +1,24 @@
+package com.livk.cloud.api;
+
+import com.livk.common.core.spring.LivkSpring;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * LivkMonitor
+ * </p>
+ *
+ * @author livk
+ * @date 2021/11/4
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class LivkMonitor {
+        public static void main(String[] args) {
+        LivkSpring.run(LivkMonitor.class, args);
+    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(LivkMonitor.class, args);
+//    }
+}
