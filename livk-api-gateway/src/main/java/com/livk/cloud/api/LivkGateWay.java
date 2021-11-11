49d6f979bea3cac80b4ce957307cf6a38be51cd3
livk1375632510 <chenyu1999>
2021-11-11 13:56:58 +0800

    refactor: log日志调整

--- livk-api-gateway/src/main/java/com/livk/cloud/api/LivkGateWay.java
+++ livk-api-gateway/src/main/java/com/livk/cloud/api/LivkGateWay.java
@@ -3,8 +3,6 @@ package com.livk.cloud.api;
 import com.livk.common.core.spring.LivkSpring;
 import com.livk.common.swagger.annotation.EnableLivkOpenApi;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
@@ -20,7 +18,5 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class LivkGateWay {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = LivkSpring.run(LivkGateWay.class, args);
-        System.out.println(context.getBean(DiscoveryClient.class));
-        System.out.println(context.getBean(RouteDefinitionRepository.class));
     }
 }
