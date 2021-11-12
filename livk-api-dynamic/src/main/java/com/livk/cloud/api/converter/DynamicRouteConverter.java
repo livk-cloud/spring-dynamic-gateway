520e86c914dd6643291a256eb3680c5dca900a18
livk1375632510 <chenyu1999>
2021-11-12 20:03:04 +0800

    refactor: 路由转换器Mapstruct调整

--- livk-api-dynamic/src/main/java/com/livk/cloud/api/converter/DynamicRouteConverter.java
+++ livk-api-dynamic/src/main/java/com/livk/cloud/api/converter/DynamicRouteConverter.java
@@ -2,10 +2,12 @@ package com.livk.cloud.api.converter;
 
 import com.livk.cloud.api.domain.DynamicRoute;
 import com.livk.cloud.api.domain.RedisRoute;
+import com.livk.cloud.api.domain.route.Filter;
+import com.livk.cloud.api.domain.route.Predicate;
 import com.livk.common.core.converter.BaseConverter;
+import com.livk.common.core.util.JacksonUtil;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.NullValueMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 /**
@@ -16,21 +18,21 @@ import org.mapstruct.factory.Mappers;
  * @author livk
  * @date 2021/11/4
  */
-@Mapper(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+@Mapper(imports = {JacksonUtil.class, Predicate.class, Filter.class})
 public interface DynamicRouteConverter extends BaseConverter<RedisRoute, DynamicRoute> {
     DynamicRouteConverter INSTANCE = Mappers.getMapper(DynamicRouteConverter.class);
 
-    @Mapping(target = "predicates", expression = "java(com.livk.common.core.util.JacksonUtil.toStream(dynamicRoute.getPredicates(),com.livk.cloud.api.domain.route.Predicate.class).toList())")
-    @Mapping(target = "filters", expression = "java(com.livk.common.core.util.JacksonUtil.toStream(dynamicRoute.getFilters(),com.livk.cloud.api.domain.route.Filter.class).toList())")
-    @Mapping(target = "metadata", expression = "java(com.livk.common.core.util.JacksonUtil.toMap(dynamicRoute.getMetadata(),String.class,Object.class))")
+    @Mapping(target = "predicates", expression = "java(JacksonUtil.toStream(dynamicRoute.getPredicates(),Predicate.class).toList())")
+    @Mapping(target = "filters", expression = "java(JacksonUtil.toStream(dynamicRoute.getFilters(),Filter.class).toList())")
+    @Mapping(target = "metadata", expression = "java(JacksonUtil.toMap(dynamicRoute.getMetadata(),String.class,Object.class))")
     @Override
     RedisRoute getSource(DynamicRoute dynamicRoute);
 
     @Mapping(target = "insertTime", ignore = true)
     @Mapping(target = "updateTime", ignore = true)
-    @Mapping(target = "predicates", expression = "java(com.livk.common.core.util.JacksonUtil.toJson(redisRoute.getPredicates()))")
-    @Mapping(target = "filters", expression = "java(com.livk.common.core.util.JacksonUtil.toJson(redisRoute.getFilters()))")
-    @Mapping(target = "metadata", expression = "java(com.livk.common.core.util.JacksonUtil.toJson(redisRoute.getMetadata()))")
+    @Mapping(target = "predicates", expression = "java(JacksonUtil.toJson(redisRoute.getPredicates()))")
+    @Mapping(target = "filters", expression = "java(JacksonUtil.toJson(redisRoute.getFilters()))")
+    @Mapping(target = "metadata", expression = "java(JacksonUtil.toJson(redisRoute.getMetadata()))")
     @Override
     DynamicRoute getTarget(RedisRoute redisRoute);
 }
