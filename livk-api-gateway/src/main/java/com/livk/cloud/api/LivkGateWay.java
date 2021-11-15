87c0936c5010367b333828d19e52740750fc6929
livk1375632510 <chenyu1999>
2021-11-15 17:18:32 +0800

    refactor:

--- livk-api-gateway/src/main/java/com/livk/cloud/api/LivkGateWay.java
+++ livk-api-gateway/src/main/java/com/livk/cloud/api/LivkGateWay.java
@@ -1,10 +1,8 @@
 package com.livk.cloud.api;
 
-import com.livk.common.core.event.LivkRemoteHandler;
 import com.livk.common.core.spring.LivkSpring;
 import com.livk.common.swagger.annotation.EnableLivkOpenApi;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * <p>
@@ -18,7 +16,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class LivkGateWay {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = LivkSpring.run(LivkGateWay.class, args);
-        System.out.println(context.getBean(LivkRemoteHandler.class));
+        LivkSpring.run(LivkGateWay.class, args);
     }
 }
