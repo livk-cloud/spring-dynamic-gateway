75c27f8b230a93efe5fdd35a6e07fff1866c59d2
livk1375632510 <chenyu1999>
2021-11-24 17:30:51 +0800

    style: spring format

--- livk-api-dynamic/src/main/java/com/livk/cloud/api/controller/RouteController.java
+++ livk-api-dynamic/src/main/java/com/livk/cloud/api/controller/RouteController.java
@@ -2,6 +2,7 @@ package com.livk.cloud.api.controller;
 
 import com.livk.cloud.api.domain.RedisRoute;
 import com.livk.cloud.api.service.DynamicRouteService;
+import com.livk.common.log.annotation.LivkLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,37 +20,37 @@ import java.util.List;
  * @date 2021/11/3
  */
 @Slf4j
-// @LivkLog
+@LivkLog
 @RestController
 @RequestMapping("route")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class RouteController {
 
-	private final DynamicRouteService dynamicRouteService;
+    private final DynamicRouteService dynamicRouteService;
 
-	@GetMapping
-	public List<RedisRoute> list() {
-		return dynamicRouteService.selectList();
-	}
+    @GetMapping
+    public List<RedisRoute> list() {
+        return dynamicRouteService.selectList();
+    }
 
-	@GetMapping("/{routeId}")
-	public RedisRoute getByRouteId(@PathVariable("routeId") String routeId) {
-		return dynamicRouteService.getById(routeId);
-	}
+    @GetMapping("/{routeId}")
+    public RedisRoute getByRouteId(@PathVariable("routeId") String routeId) {
+        return dynamicRouteService.getById(routeId);
+    }
 
-	@RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
-	public Boolean addNewRoute(@RequestBody @Valid RedisRoute redisRoute) {
-		return dynamicRouteService.saveOrUpdate(redisRoute);
-	}
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+    public Boolean addNewRoute(@RequestBody @Valid RedisRoute redisRoute) {
+        return dynamicRouteService.saveOrUpdate(redisRoute);
+    }
 
-	@DeleteMapping("/{routeId}")
-	public Boolean deleteRoute(@PathVariable("routeId") String routeId) {
-		return dynamicRouteService.delete(routeId);
-	}
+    @DeleteMapping("/{routeId}")
+    public Boolean deleteRoute(@PathVariable("routeId") String routeId) {
+        return dynamicRouteService.delete(routeId);
+    }
 
-	@PostMapping("reload")
-	public Boolean reload() {
-		return dynamicRouteService.reload();
-	}
+    @PostMapping("reload")
+    public Boolean reload() {
+        return dynamicRouteService.reload();
+    }
 
 }
