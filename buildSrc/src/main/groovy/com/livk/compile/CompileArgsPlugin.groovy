a40cfc16eb827cfe7af3f56bb849f21024956377
livk <1375632510@qq.com>
2022-07-23 11:22:28 +0800

    feat

--- buildSrc/src/main/groovy/com/livk/compile/CompileArgsPlugin.groovy
+++ buildSrc/src/main/groovy/com/livk/compile/CompileArgsPlugin.groovy
@@ -19,7 +19,6 @@ import org.gradle.api.tasks.testing.Test
 abstract class CompileArgsPlugin implements Plugin<Project> {
 
     private static final List<String> COMPILER_ARGS = new ArrayList<>()
-    private static final String MAPSTRUCT_NAME = "mapstruct"
     private static final String MAPSTRUCT_PROCESSOR_NAME = "mapstruct-processor"
     private static final List<String> MAPSTRUCT_COMPILER_ARGS = new ArrayList<>()
     private static final String UTF_8 = "UTF-8"
@@ -53,7 +52,7 @@ abstract class CompileArgsPlugin implements Plugin<Project> {
             project.configurations.forEach {
                 dependencyName.addAll(it.dependencies.name)
             }
-            if (dependencyName.contains(MAPSTRUCT_NAME) || dependencyName.contains(MAPSTRUCT_PROCESSOR_NAME)) {
+            if (dependencyName.contains(MAPSTRUCT_PROCESSOR_NAME)) {
                 javaCompile.options.compilerArgs.addAll(MAPSTRUCT_COMPILER_ARGS)
             }
         }
