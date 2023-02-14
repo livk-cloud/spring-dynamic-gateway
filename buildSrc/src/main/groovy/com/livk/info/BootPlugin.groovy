f96902c0e1910bcddcac9d87cf1172eee38ef27e
livk <1375632510@qq.com>
2023-02-14 11:49:26 +0800

    feat: 版本升级

--- buildSrc/src/main/groovy/com/livk/info/BootPlugin.groovy
+++ buildSrc/src/main/groovy/com/livk/info/BootPlugin.groovy
@@ -9,6 +9,7 @@ import org.springframework.boot.gradle.plugin.SpringBootPlugin
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 import java.time.Instant
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeUnit
 
 /**
@@ -24,19 +25,18 @@ abstract class BootPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.pluginManager.apply(JavaPlugin.class)
         project.pluginManager.apply(SpringBootPlugin.class)
-        project.extensions
-                .getByType(SpringBootExtension.class)
+        project.extensions.getByType(SpringBootExtension.class)
                 .buildInfo {
-                    it.properties {
-                        it.group = project.group
-                        it.version = project.version
-                        it.time = Instant.now().plusMillis(TimeUnit.HOURS.toMillis(8))
+                    it.properties { build ->
+                        build.group.set(project.group.toString())
+                        build.version.set(project.version.toString())
+                        build.time.set(DateTimeFormatter.ISO_INSTANT.format(Instant.now().plusMillis(TimeUnit.HOURS.toMillis(8))))
                     }
                 }
-        def bootJar = project.tasks.getByName(SpringBootPlugin.BOOT_JAR_TASK_NAME) as BootJar
+        def bootJar = project.tasks.named(SpringBootPlugin.BOOT_JAR_TASK_NAME).get() as BootJar
         bootJar.archiveBaseName.set(project.name)
         bootJar.archiveFileName.set(bootJar.archiveBaseName.get() + "." + bootJar.archiveExtension.get())
         bootJar.launchScript()
-        (project.tasks.getByName(JavaPlugin.JAR_TASK_NAME) as Jar).enabled = false
+        (project.tasks.named(JavaPlugin.JAR_TASK_NAME).get() as Jar).enabled = false
     }
 }
